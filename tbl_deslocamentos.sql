1bef86afb10cc575f38fd4cb28e7e8246576cc97
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:42:51 -0300

    Add files via upload

---
+++ tbl_deslocamentos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_deslocamentos` (
+  `idtbl_deslocamentos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_deslocamentoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_deslocamentos`));
