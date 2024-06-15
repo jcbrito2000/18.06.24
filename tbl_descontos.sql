1150a2681850f51e4b1bbdb8534adf9d15f87782
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:50:51 -0300

    Add files via upload

---
+++ tbl_descontos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_descontos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_descontoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
