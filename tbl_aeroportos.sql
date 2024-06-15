dfbf617be8f0e2f8f506ba35a24a5a6778b2c8c2
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:56:45 -0300

    Add files via upload

---
+++ tbl_aeroportos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_aeroportos` (
+  `idtbl_aeroportos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_aeroportoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_aeroportos`));
