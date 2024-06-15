8cd2b47cf03b77643dd037fdd6b7d514fb9bd74e
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:35:19 -0300

    Add files via upload

---
+++ tbl_milhas.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_milhas` (
+  `idtbl_milhas` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_milhascol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_milhas`));
