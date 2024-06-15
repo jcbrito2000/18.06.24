e84e33b67a89916e0fdc2ba6cb91db85a108aa8e
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:53:31 -0300

    Add files via upload

---
+++ tbl_tipos_enderecos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_tipos_enderecos` (
+  `idtbl_tipos_enderecos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_tipos_enderecoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_tipos_enderecos`));
