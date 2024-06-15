5e2b5342f2cc1b1d85a91d4eb7279e8212446ba8
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:06:36 -0300

    Add files via upload

---
+++ tbl_orcamentos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_orcamentos` (
+  `idtbl_orcamentos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_orcamentoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_orcamentos`));
