b5d2f5f6751c4e70c0290ff7baa1f9e49784c783
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:53:45 -0300

    Add files via upload

---
+++ tbl_rodoviarias.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_rodoviarias` (
+  `idtbl_rodoviarias` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_rodoviariascol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_rodoviarias`));
