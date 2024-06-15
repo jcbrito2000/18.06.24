1505d5719785cc74b457858528d1ceddaea7a1e8
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:08:54 -0300

    Add files via upload

---
+++ tbl_pedidos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_pedidos` (
+  `idtbl_pedidos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_pedidoscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_pedidos`));
