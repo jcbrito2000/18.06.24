e9b250c6ed818a42929c91a789935da0dcd4b53b
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:27:46 -0300

    Add files via upload

---
+++ tbl_tradutores.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_tradutores` (
+  `idtbl_tradutores` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_tradutorescol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_tradutores`));
