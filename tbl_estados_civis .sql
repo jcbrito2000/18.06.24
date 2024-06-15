1719a10e1e3712df67fce3814724214b3e315593
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:49:17 -0300

    Add files via upload

---
+++ tbl_estados_civis .sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_estados_civis` (
+  `idtbl_estados_civis` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_estados_civiscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_estados_civis`));
