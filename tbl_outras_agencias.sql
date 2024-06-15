55963b6c83a14ec8b94fd97f7d0c9816f2c55164
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:22:51 -0300

    Add files via upload

---
+++ tbl_outras_agencias.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_outras_agencias` (
+  `idtbl_outras_agencias` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_outras_agenciascol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_outras_agencias`));
