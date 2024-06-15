4072051257f1c38fba9293b16b86f31c75626b30
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:36:33 -0300

    Add files via upload

---
+++ tbl_paradas_de_viagem.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_paradas_de_viagem` (
+  `idtbl_paradas_de_viagem` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_paradas_de_viagemcol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_paradas_de_viagem`));
