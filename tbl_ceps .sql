19ca9715566cd399a2aa6ca368b700605185e45a
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 01:55:54 -0300

    Add files via upload

---
+++ tbl_ceps .sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_ceps` (
+  `idtbl_ceps` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_cepscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_ceps`));
