26941e59e670c311a46bff7433bfe70c3cbcbac5
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:32:39 -0300

    Add files via upload

---
+++ tbl_turistaas.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_turistas` (
+  `idtbl_turistas` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_turistascol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_turistas`));
