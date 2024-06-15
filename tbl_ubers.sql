338ce6eb9a2053681525517f8ebae7044f4f60dd
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-15 00:58:57 -0300

    Add files via upload

---
+++ tbl_ubers.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_ubers` (
+  `idtbl_ubers` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `tbl_uberscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtbl_ubers`));
